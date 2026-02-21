a851ca0c36a068427f8fbc616691138890d90480
rythm-sachdeva <rythmrko123@gmail.com>
2026-02-21 09:02:36 +0530

    fix(docker): fixed docker compose file

--- social_scheduler/settings.py
+++ social_scheduler/settings.py
@@ -224,3 +224,15 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.StaticCloudinaryStorage",
     },
 }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST', default='db'),
+        'PORT': env('DB_PORT', default='5432'),
+    }
+}
